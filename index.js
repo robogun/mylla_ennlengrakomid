4c89097ca48902cf8af1737013542cc4295d16bc
robogun <32485165+robogun@users.noreply.github.com>
2020-04-19 16:35:44 +0000

    check

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // undirbúningsskipanir
 var app = require('express')();
-var http = require('http').Server(app);
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000
 
